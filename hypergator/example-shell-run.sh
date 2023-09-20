dbe164ce5dab468aa06ecb1bfb33c2ed66e35f18
JT Miller <jtmiller@ucsb.edu>
2023-09-20 16:49:27 -0400

    add

--- hypergator/example-shell-run.sh
+++ hypergator/example-shell-run.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1                        # Run all processes on a single node
 #SBATCH --ntasks=1                       # Run a single task
 #SBATCH --cpus-per-task=1                # Number of CPU cores per task
-#SBATCH --mem=10gb                        # Job memory request
+#SBATCH --mem=3gb                        # Job memory request
 #SBATCH --time=00-01:00:00               # Time limit days-hrs:min:sec
 #SBATCH --qos=soltis-b
 pwd; hostname; date
@@ -17,6 +17,6 @@ pwd; hostname; date
 module load R/4.2
 
 #do some (or alot) of coding
-Rscript --vanilla /blue/soltis/millerjared/pollen-project/Pollen-DB/pollen-project-cluster/plant-parallel-script-SiTsTm.R $1 $2 
+Rscript --vanilla /blue/soltis/millerjared/useful-code-depository/hypergator/example-script-to-run.R $1 $2 
 
-##example for running: sbatch /blue/soltis/millerjared/useful-code-depository/hypergator/example-shell-run.sh /blue/soltis/millerjared/hypergator/data/ /blue/soltis/millerjared/hypergator/plots/
+##example for running: sbatch /blue/soltis/millerjared/useful-code-depository/hypergator/example-shell-run.sh /blue/soltis/millerjared/useful-code-depository/data/ /blue/soltis/millerjared/useful-code-depository/plots/
